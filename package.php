45674e9cadad5d4849bf406d072e1aa796d22a1b
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2010-06-19 12:39:38 +0900

    - Added the packaging script.

---
+++ package.php
@@ -0,0 +1,86 @@
+<?php
+require_once 'PEAR/PackageFileManager2.php';
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+date_default_timezone_set('UTC');
+
+// general package information
+$channel        = '__uri';
+$packagename    = 'mecab';
+$summary        = 'The PHP bindings of the MeCab.';
+
+$description    = $summary;
+
+// information of cureent version
+$version        = '0.4.1';
+$apiversion     = '0.4.0';
+$stability      = 'beta';
+$apistability   = 'beta';
+
+$notes = <<<EOS
+- Initial release on PEAR channel.
+EOS;
+
+// set parameters to the package
+$packagexml = new PEAR_PackageFileManager2;
+$packagexml->setOptions(array(
+    //'packagefile'       => 'package2.xml',
+    'baseinstalldir'    => '/',
+    'packagedirectory'  => dirname(__FILE__),
+    'filelistgenerator' => 'file',
+    'ignore'    => array(
+        'package.php',
+        'package.xml',
+        "{$packagename}-{$version}.tgz"),
+    'dir_roles' => array(
+        'tests'     => 'test',
+        'manual'    => 'doc',
+        'examples'  => 'data'),
+    'exceptions'    => array(
+        "{$packagename}.dsp"    => 'src',
+        'CREDITS'       => 'doc',
+        'EXPERIMENTAL'  => 'doc',
+        'LICENSE'       => 'doc',
+        'README'        => 'doc')));
+
+$packagexml->setPackage($packagename);
+$packagexml->setSummary($summary);
+$packagexml->setNotes($notes);
+$packagexml->setDescription($description);
+$packagexml->setLicense('MIT License', 'http://www.opensource.org/licenses/mit-license.php');
+
+$packagexml->setReleaseVersion($version);
+$packagexml->setAPIVersion($apiversion);
+$packagexml->setReleaseStability($stability);
+$packagexml->setAPIStability($apistability);
+
+$packagexml->addMaintainer('lead', 'rsky', 'Ryusuke SEKIYAMA,', 'rsky0711@ozmm.org');
+
+$packagexml->setPackageType('extsrc');
+$packagexml->setProvidesExtension($packagename);
+$packagexml->addConfigureOption('with-mecab', 'specify pathname to mecab-config', 'no');
+$packagexml->setPhpDep('5.2.0');
+$packagexml->setPearinstallerDep('1.4.1');
+
+$packagexml->setChannel($channel);
+$packagexml->generateContents();
+
+// get a PEAR_PackageFile object
+//$packagexml1 = &$packagexml->exportCompatiblePackageFile1();
+
+// generate package.xml
+if (php_sapi_name() == 'cli' && $argc > 1 && $argv[1] == 'make') {
+    $make = true;
+} elseif (!empty($_GET['make'])) {
+    $make = true;
+} else {
+    $make = false;
+}
+// note use of debugPackageFile() - this is VERY important
+if ($make) {
+    //$packagexml1->writePackageFile();
+    $packagexml->writePackageFile();
+    $data = file_get_contents('package.xml');
+} else {
+    //$packagexml1->debugPackageFile();
+    $packagexml->debugPackageFile();
+}
